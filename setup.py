e2394981d296994d929f023a542040abc44ad4a1
Mickael Desfrenes <desfrenes@gmail.com>
2011-12-05 13:50:45 +0100

    added classifiers

--- setup.py
+++ setup.py
@@ -5,5 +5,13 @@ setup(
         description="Python implementation of RedBeanPHP, easy to use ORM.",
         author="Mickael Desfrenes",
         author_email="desfrenes@gmail.com",
-        py_modules=["pybean"]
+        py_modules=["pybean"],
+        classifiers=[
+            "Development Status :: 2 - Pre-Alpha",
+            "Intended Audience :: Developers",
+            "Topic :: Database :: Front-Ends",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+            "License :: OSI Approved :: BSD License",
+            "Programming Language :: Python",
+            ]
 )
